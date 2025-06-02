1fbf5eed49a2c4d49033da38894bfe8ef058a74f
Gump Yi <gump.yi@outlook.com>
2025-06-02 23:36:23 +0800

    upload to S3 bucket

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,16 @@
-import { notice } from "@actions/core";
+import core from "@actions/core";
 import github from "@actions/github";
 import exec from "@actions/exec";
 
 function run() {
-  notice("Hello from my custom JavaScript Action!");
+  const bucket = core.getInput("bucket", { required: true });
+  const region = core.getInput("region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`);
+
+  core.notice("Hello from my custom JavaScript Action!");
 }
 
 run();
